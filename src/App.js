8897ba617dfb0e80fb159144aca8cd1f63cd161f
Tomilade18 <jboyyumzey@gmail.com>
2025-08-02 22:10:45 +0100

    exercise completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <main>
+      <div className="card">
+        <img 
+          className="header-img"
+          src="https://tse4.mm.bing.net/th/id/OIP.0ku1AvUo__-ohxH4EqTXOAHaEo?rs=1&pid=ImgDetMain&o=7&rm=3" 
+          alt="" 
+        />
+        <p className="tag">Learning</p>
+        <p>Published 21 Dec 2013</p>
+        <h1>HTML and CSS Foundations</h1>
+        <p className="content">These languages are the backbone of every website, defining content, structure, and presentation</p>
+
+        <div className="profile">
+          <img className='profile-img' src="/images/image-avatar.webp" alt="" />
+          <p className='name'>Greg Hooper</p>
+        </div>
+      </div>
+   </main>
   );
 }
 
